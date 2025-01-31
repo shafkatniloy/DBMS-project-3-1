a26c8d7fbca1c257ec79b35f6953325cfc37204b
Safkat Islam Niloy <164136080+shafkatniloy@users.noreply.github.com>
2025-01-31 15:37:26 +0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','eshan','courier_m')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','admin12','courier_m')or die("Could not connect to mysql".mysqli_error($con));
